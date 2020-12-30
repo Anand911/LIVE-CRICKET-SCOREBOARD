68c94ad288e72613bc24260beac21d273e57cb2e
Anand Ajith <anandajith911@gmail.com>
2020-12-31 00:32:26 +0530

    okay

--- creds.py
+++
@@ -1,5 +0,0 @@
-from pickle import dump
-creds={'userid':'pyadmin','password':'pycharm123'}
-with open('admin.dat','wb') as u_file:
-	dump(creds, u_file)
-
--- test.py
+++
@@ -1,7 +0,0 @@
-from pickle import load
-mcode='PeAJ'
-fname='innings/1innings_'+mcode+'.txt'
-with open(fname,'rb') as ifile:
-    stats=load(ifile)
-match_stats1,bat_stats1,bowl_stats1=stats
-print(match_stats1)
